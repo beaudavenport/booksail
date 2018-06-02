10286e2a5a9d069494e9f0c2919cb0800e2eb246
Beau Davenport <beau.davenport@asynchrony.com>
2018-06-02 14:32:05 -0500

    Add initial Expo project scaffolding

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import BooksScreen from '../screens/BooksScreen';
+import ListingsScreen from '../screens/ListingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const BooksStack = createStackNavigator({
+  Books: BooksScreen,
+});
+
+BooksStack.navigationOptions = {
+  tabBarLabel: 'Books',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+    />
+  ),
+};
+
+const ListingsStack = createStackNavigator({
+  Listings: ListingsScreen,
+});
+
+ListingsStack.navigationOptions = {
+  tabBarLabel: 'Listings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  BooksStack,
+  ListingsStack,
+});
