de611f62c152ed7af3efeeaeb5e1bea85c46f7c7
Beau Davenport <beau.davenport@asynchrony.com>
2018-08-02 09:42:18 -0500

    Add proof-of-concept for fetching access token and using refresh token

--- screens/ListingsScreen.js
+++ screens/ListingsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Button, View } from 'react-native';
+import { Text, Button, ScrollView } from 'react-native';
 import { AuthSession } from 'expo';
 import config from '../config';
 
@@ -9,24 +9,60 @@ export default class ListingsScreen extends React.Component {
   };
 
   state = {
-    tokenResult: null,
+    requestTokenResult: null,
+    accessToken: null,
+    refreshToken: null,
   };
 
  handleLogin = () => {
    AuthSession.startAsync({
      authUrl: config.authUrl,
-   }).then((result) => {
-     this.setState({ tokenResult: result });
-   });
+   }).then((requestTokenResponse) => {
+     console.log({ requestTokenResponse });
+     this.setState({ requestTokenResult: requestTokenResponse.params.code });
+     return fetch(config.accessTokenUrl, {
+       headers: {
+         code: requestTokenResponse.params.code,
+       },
+     });
+   }).then(accessTokenResponse => accessTokenResponse.json())
+     .then((accessTokenResult) => {
+       console.log({ accessTokenResult });
+       this.setState({
+         accessToken: accessTokenResult.access_token,
+         refreshToken: accessTokenResult.refresh_token,
+       });
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+ }
+
+ refreshToken = () => {
+   fetch(config.refreshTokenUrl, {
+     headers: {
+       rfToken: this.state.refreshToken,
+     },
+   }).then(refreshTokenResponse => refreshTokenResponse.json())
+     .then((refreshTokenResult) => {
+       console.log({ refreshTokenResult });
+       this.setState({ accessToken: refreshTokenResult.access_token });
+     })
+     .catch((err) => {
+       console.log(err);
+     });
  }
 
  render() {
    return (
-     <View>
+     <ScrollView>
        <Text>Sign in with Ebay to get started</Text>
        <Button title="Sign in" onPress={this.handleLogin} />
-       <Text>{JSON.stringify(this.state.tokenResult)}</Text>
-     </View>
+       <Text>{JSON.stringify(this.state.requestTokenResult)}</Text>
+       <Text>{JSON.stringify(this.state.accessToken)}</Text>
+       <Text>{JSON.stringify(this.state.refreshToken)}</Text>
+       <Button title="Call to refresh" onPress={this.refreshToken} />
+     </ScrollView>
    );
  }
 }
