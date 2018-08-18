0a7837186789e116a7ebb155bc4a0a2246f5b230
Beau Davenport <beau.davenport@asynchrony.com>
2018-08-18 14:09:26 -0500

    Return access token and refresh token from auth login method

--- __tests__/Auth.spec.js
+++ __tests__/Auth.spec.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import { AuthSession } from 'expo';
-import { login } from '../services/auth';
+import auth from '../services/auth';
 import config from '../config';
 
 describe('Auth Service', () => {
   describe('login', () => {
     beforeEach(() => {
       AuthSession.startAsync = jest.fn();
+      global.fetch = jest.fn();
+
+      AuthSession.startAsync.mockResolvedValue({ params: {} });
+      fetch.mockResolvedValue({ json: () => Promise.resolve({}) });
     });
 
     it('should call to auth session with auth url set', () => {
-      login();
+      const asyncResult = auth.login();
 
-      expect(AuthSession.startAsync.mock.calls[0][0])
-        .toEqual({ authUrl: config.authUrl });
+      return asyncResult.then(() => {
+        expect(AuthSession.startAsync.mock.calls[0][0])
+          .toEqual({ authUrl: config.authUrl });
+      });
+    });
+
+    it('should call to fetch tokens using request token', () => {
+      const requestToken = 'request-token';
+      const accessToken = 'access-token';
+      const refreshToken = 'refresh-token';
+      AuthSession.startAsync
+        .mockResolvedValue({ params: { code: requestToken } });
+      const accessTokenResponse = {
+        json: () => Promise.resolve({
+          access_token: accessToken,
+          refresh_token: refreshToken,
+        }),
+      };
+      fetch.mockResolvedValue(accessTokenResponse);
+
+      const asyncResult = auth.login();
+
+      return asyncResult.then((accessTokenResult) => {
+        expect(accessTokenResult).toEqual({ accessToken, refreshToken });
+
+        expect(fetch.mock.calls[0][0])
+          .toEqual(config.accessTokenUrl);
+        expect(fetch.mock.calls[0][1])
+          .toEqual({ headers: { code: requestToken } });
+      });
     });
   });
 });
--- services/auth.js
+++ services/auth.js
@@ -1,8 +1,22 @@
 import { AuthSession } from 'expo';
 import config from '../config';
 
-export function login() {
-  AuthSession.startAsync({
+function login() {
+  return AuthSession.startAsync({
     authUrl: config.authUrl,
-  });
+  })
+    .then(authResponse => fetch(config.accessTokenUrl, {
+      headers: {
+        code: authResponse.params.code,
+      },
+    }))
+    .then(response => response.json())
+    .then(result => ({
+      accessToken: result.access_token,
+      refreshToken: result.refresh_token,
+    }));
 }
+
+export default {
+  login,
+};
